7877b20b66e87063e20bc2c3750516b728629a6b
Aayush Kantak <120012051+aysh01@users.noreply.github.com>
2024-12-21 17:29:33 +0530

    Add files via upload

--- first_app.py
+++ first_app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for, redirect, session, j
 from flask import Blueprint
 from pymongo import MongoClient
 from datetime import datetime
+import random as rd
 
 db_password = "6ruyipcr9R1sCSGh"
 
@@ -11,10 +12,13 @@ client = MongoClient(
     f"mongodb+srv://aayushkantak01:{db_password}@cluster0.ylmey.mongodb.net/")
 db = client['mydb']
 collection1 = db['food_items']
+collection2 = db['monthly-bills']
 
 
 def get_monthly_collection():
     current_month = datetime.now().strftime('%Y_%m')
+    month = datetime.now().strftime('%m')
+    print(current_month, "\n", month)
     return db[f'food_items_{current_month}']
 
 
@@ -49,10 +53,10 @@ def add_food_items():
         'price': price,
         'total': total
     }
-    
+
     collection1 = get_monthly_collection()
     existing_item = collection1.find_one({'barcode': barcode})
-    
+
     if existing_item:
         return jsonify({'message': 'Barcode already exists in the database.'}), 200
     else:
@@ -64,3 +68,70 @@ def add_food_items():
             return jsonify({'exists': True, 'message': 'Barcode, is required!'}), 400
         else:
             return jsonify({'exists': False, 'message': 'Data saved successfully!'}), 200
+
+
+@monthly_bill.route('/view_monthly_bill', methods=['GET'])
+def view_monthly_bill():
+    existing_item = collection2.find_one(
+        {'Paid On': {'$exists': True, '$ne': ''}})
+
+    if existing_item:
+        invoice_no = existing_item.get('Invoice No')
+        return render_template("bills_paid.html", paid=session['paid'][1],
+                               month=session.get('month'), date=session.get('date'),
+                               invoice=invoice_no)
+    else:
+        collection1 = get_monthly_collection()
+        month = datetime.now().strftime("%B")
+        date = datetime.now().strftime("%B %d, %Y")
+        time = datetime.now().strftime("%I:%M:%S %p")
+        print(date)
+        invoice = x = rd.randint(1000, 99999)
+        items = list(collection1.find())
+
+        for item in items:
+            item['quantity'] = int(item['quantity'])  # Convert to int
+            item['price'] = int(item['price'])
+
+            total_cost = sum(int(item['quantity']) *
+                             float(item['price']) for item in items)
+
+            paid = ['Paid', 'already Paid']
+            session['paid'] = paid
+
+            bills = {
+                'Invoice Date': date,
+                'Invoice No': invoice,
+                'Bill Month': month,
+                'Due By': date,
+                'Paid On': time,
+                'Sub Total': total_cost
+            }
+
+            session['bills'] = bills
+            session['month'] = month
+            session['total_cost'] = total_cost
+            session['date'] = date
+            session['invoice'] = invoice
+
+            return render_template('view_monthly_bill.html', items=items, total_cost=total_cost,
+                                   month=month, time=time, date=date, invoice=invoice)
+
+
+@monthly_bill.route('pay', methods=['POST'])
+def pay():
+    bills = session.get('bills')
+    total = session.get('total_cost')
+    existing_item = collection2.find_one(
+        {'Sub Total': total})
+
+    if existing_item:
+        invoice_no = existing_item.get('Invoice No')
+        return render_template("bills_paid.html", paid=session['paid'][1],
+                               month=session.get('month'), date=session.get('date'),
+                               invoice=invoice_no)
+    else:
+        collection2.insert_one(bills)
+        return render_template("bills_paid.html", paid=session['paid'][0],
+                               month=session.get('month'), date=session.get('date'),
+                               invoice=session.get('invoice'))
